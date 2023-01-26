a944e9b549d20b076a8f975256fe6bf79f961ed8
Saikat Chakraborty <saikatgpae@gmail.com>
2023-01-26 15:11:09 +0530

    Remove linters error.

--- morse_code.rb
+++ morse_code.rb
@@ -1,27 +1,26 @@
 def decode_char(char)
-    morse_dict = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-      '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-      '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S',
-      '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
-    }
-    morse_dict[char]
-  end
-  puts decode_char('.-') #=> A
-  def decode_word(arr)
-    word = ''
-    arr.split.each { |i| word += decode_char(i) }
-    word
-  end
-  puts decode_word('-- -.--') #=> MY
+  morse_dict = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S',
+    '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }
+  morse_dict[char]
+end
+puts decode_char('.-') #=> A
+def decode_word(arr)
+  word = ''
+  arr.split.each { |i| word += decode_char(i) }
+  word
+end
+puts decode_word('-- -.--') #=> MY
 
-  def decode(str)
-    sentese = ''
-    arry = str.split('   ')
-    arry.each { |i| sentese += "#{decode_word(i)} " }
-    sentese
-  end
-  puts decode("-- -.--   -. .- -- .") #=> MY NAME
-  x = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-  puts x
-  
+def decode(str)
+  sentese = ''
+  arry = str.split('   ')
+  arry.each { |i| sentese += "#{decode_word(i)} " }
+  sentese
+end
+puts decode('-- -.--   -. .- -- .') #=> MY NAME
+x = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts x
